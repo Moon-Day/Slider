84aa22b2a34e1f1ac336c70677ca2dc2fd39a08e
Guilherme de Souza <97771245+Moon-Day@users.noreply.github.com>
2023-05-13 15:47:03 -0300

    comentarios

--- src/script.js
+++ src/script.js
@@ -2,16 +2,16 @@ const slider = document.querySelectorAll('.slider');
 const btnPrev = document.getElementById('prev-button');
 const btnNext = document.getElementById('next-button');
 
-let currentSlide = 0;
+let currentSlide = 0; // slide atual 
 
-function hideSlider() {
+function hideSlider() { // remover a class on do slide
   slider.forEach(item => item.classList.remove('on'))
 }
 
-function showSlider() {
+function showSlider() { //adicionar a class on no slide
   slider[currentSlide].classList.add('on')
 }
-
+// As funções "nextSlider" e "prevSlider" são criadas para avançar e voltar os slides, respectivamente. A função "nextSlider" começa chamando a função "hideSlider", checa se o slide atual é o último slide da lista utilizando um condicional "if" e, se for, define o slide atual como o primeiro slide da lista. Caso contrário, incrementa o valor de "currentSlide". Em seguida, a função "showSlider" é chamada para exibir o slide atual. A função "prevSlider" é semelhante, mas decrementa o valor de "currentSlide" em vez de incrementá-lo e define o último slide como o slide atual caso o slide atual seja o primeiro slide da lista.
 function nextSlider() {
   hideSlider()
   if(currentSlide === slider.length -1) {
@@ -31,6 +31,12 @@ function prevSlider() {
   }
   showSlider()
 }
+
+// Por fim, os eventos "click" são adicionados aos botões "prev-button" e "next-button" com as funções "prevSlider" e "nextSlider" respectivamente, utilizando o método "addEventListener". Quando um dos botões é clicado, a função correspondente é chamada e o slide atual é atualizado.
+
+
+
+
 
 btnNext.addEventListener('click', nextSlider)
 btnPrev.addEventListener('click', prevSlider)
